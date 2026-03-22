955ae407158123e03bccfde599a23d22408019d8
Oleksander Hodovaniuk <s.godovanuk@gmail.com>
2026-03-22 16:43:50 +0200

    Add initial ApiGateway

---
+++ backend/ApiGateway/Program.cs
@@ -0,0 +1,23 @@
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Configuration.AddJsonFile("ocelot.json", optional: false, reloadOnChange: true);
+
+builder.Services.AddOcelot(builder.Configuration);
+
+builder.Services.AddHttpClient("ocelot")
+    .ConfigurePrimaryHttpMessageHandler(() => new HttpClientHandler
+    {
+        ServerCertificateCustomValidationCallback =
+            HttpClientHandler.DangerousAcceptAnyServerCertificateValidator
+    });
+
+var app = builder.Build();
+
+app.UseHttpsRedirection();
+
+await app.UseOcelot();
+
+app.Run();
